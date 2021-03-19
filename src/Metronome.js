4dc4d0e79615296ced36d9037eb5c27eb68d908f
Lukasz Smutek <lukaszsmutek@gmail.com>
2021-03-19 23:58:00 +0100

    Modify sound files

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Metronome.css';
+import click1 from "./sounds/click1.wav";
+import click2 from "./sounds/click2.wav";
 
 class Metronome extends Component {
 
